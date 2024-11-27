4e87fc3d6b987553e0438ba2ffeb4bdf47f037e6
trvds <tiagovs02@gmail.com>
2024-11-27 19:00:05 +0000

    Fix v4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/letter-assigner',
+  base: '/letter-assigner/',
   plugins: [react()]
 })
