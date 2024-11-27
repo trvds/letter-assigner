72383517b43f0830f1acc15edb052d7bf4504fea
trvds <tiagovs02@gmail.com>
2024-11-27 18:36:34 +0000

    Fix page url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/letter_assigner/',
+  base: '/letter-assigner/',
   plugins: [react()]
 })
